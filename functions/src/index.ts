20b3aeb4ed965e5d07eed9342499eec5f8f3bc15
IPG <ipg@IPGnoMacBook-puro.local>
2019-04-29 19:32:18 +0900

    feature: fix access token bug

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,23 @@
 import * as functions from 'firebase-functions';
 import axios from 'axios'
 
+const baseUrl = 'https://api.github.com/repos/soniczsonic/wMaker/contents/README.md'
+const myAccessToken = '731e8370810ddfeb4ff97a1fd36122bf8b94cb7a'
+let successMessage = 'success to update readme yay'
+
 export const updateReadme = functions.https.onRequest(async(request, response) => {
-  const baseUrl = 'https://api.github.com/repos/soniczsonic/wMaker/contents/README.md'
   try {
     const res = await axios.get(baseUrl)
     const base64DateStr = await Buffer.from(JSON.stringify(new Date())).toString('base64')
-    axios.put(`${baseUrl}?access_token=a1733aa9aa9a5ea62f2c226d5fa7ed475c5f9395`, {
-      "message": "wwwww",
+    axios.put(`${baseUrl}?access_token=${myAccessToken}`, {
+      "message": "gardening is fun! I love github!",
       "content": base64DateStr,
       "sha": res.data.sha
+    }).catch(error => {
+      successMessage = 'error at put request'
     })
-    response.send('success to update readme')
+
+    response.send(successMessage)
   } catch (e) {
     response.send('error at getShaOfReadme');
   }
